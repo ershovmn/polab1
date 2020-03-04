c9da889a80c0ec5886e040d8fa1b241ea86e4dbf
tehnarenok <tehnarenok@gmail.com>
2020-03-04 15:24:49 +0300

    find max element in array

--- tests/forTests.py
+++ tests/forTests.py
@@ -60,6 +60,24 @@ class SortArray:
             element {any} -- last element
         """        
         return self._data.pop()
+    
+    def max(self):
+        """Find max in array
+        
+        Returns:
+            element {any} -- max element
+        """        
+        if(len(self._data) == 0):
+            return None
+        
+        first = self._data[0]
+        last = self._data[-1]
+
+        if(first > last):
+            return first
+        else:
+            return last
+        
 
 if __name__ == '__main__':
     a = SortArray()
